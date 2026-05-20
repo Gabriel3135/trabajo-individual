2b4ad778e4e425122b062da0ee1957b54e16b586
gabriel <12montecino@gmail.com>
2026-05-20 08:44:24 -0300

    hola a todos

---
+++ saludo.py
@@ -0,0 +1,10 @@
+def saludar(nombre):
+    print(f"Hola, {nombre}!")
+
+def despedir(nombre):
+    print(f"Chau, {nombre}!")
+
+if __name__ == "__main__":
+    saludar("Mundo")
+    despedir("Mundo")
+
