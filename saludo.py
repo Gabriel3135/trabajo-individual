4d5ad35eae0bac40cf9ba4b6298155a5c07d0666
Pao <89752372+paobtorres@users.noreply.github.com>
2026-06-29 18:19:39 -0300

    Change farewell message from 'Chau' to 'Adios'

--- saludo.py
+++ saludo.py
@@ -2,7 +2,7 @@ def saludar(nombre):
     print(f"¡Hola y buen día, {nombre}!")
 
 def despedir(nombre):
-    print(f"Chau, {nombre}!")
+    print(f"Adios, {nombre}!")
 
 if __name__ == "__main__":
     saludar("Mundo")
