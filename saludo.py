d888dd8750766bb49ec00c7b992087e895cfb77f
Pao <89752372+paobtorres@users.noreply.github.com>
2026-06-29 18:21:15 -0300

    Change farewell message to 'Buenas Noches'

--- saludo.py
+++ saludo.py
@@ -2,7 +2,7 @@ def saludar(nombre):
     print(f"¡Hola y buen día, {nombre}!")
 
 def despedir(nombre):
-    print(f"Adios, {nombre}!")
+    print(f"Buenas Noches, {nombre}!")
 
 if __name__ == "__main__":
     saludar("Mundo")
