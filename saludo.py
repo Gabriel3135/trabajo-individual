ef2ff5a8f178998bb0c2e3e99ab927be63d0ad5b
Pao <89752372+paobtorres@users.noreply.github.com>
2026-06-29 18:21:31 -0300

    Change farewell message to 'Hasta Luego'

--- saludo.py
+++ saludo.py
@@ -2,7 +2,7 @@ def saludar(nombre):
     print(f"¡Hola y buen día, {nombre}!")
 
 def despedir(nombre):
-    print(f"Adios, {nombre}!")
+    print(f"Hasta Luego, {nombre}!")
 
 if __name__ == "__main__":
     saludar("Mundo")
